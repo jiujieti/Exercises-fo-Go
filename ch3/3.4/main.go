d896e7a9406e04c3a472272908d34d00aab74ab9
jiujieti <melody2014ymq@gmail.com>
2018-01-22 23:16:37 +0100

    3.4 added

---
+++ ch3/3.4/main.go
@@ -0,0 +1,93 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 21.
+
+// Server3 is an "echo" server that displays request parameters.
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"net/http"
+)
+
+const (
+	width, height = 600, 320            // canvas size in pixels
+	cells         = 100                 // number of grid cells
+	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
+	xyscale       = width / 2 / xyrange // pixels per x or y unit
+	zscale        = height * 0.4        // pixels per z unit
+	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+)
+
+var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+//!+handler
+// handler echoes the HTTP request.
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "image/svg+xml")
+	surface(w)
+}
+
+func surface(out io.Writer) {
+	buffer := new(bytes.Buffer)
+	s := fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", width, height)
+	buffer.WriteString(s)
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay, _ := corner(i+1, j)
+			bx, by, peak := corner(i, j)
+			cx, cy, _ := corner(i, j+1)
+			dx, dy, _ := corner(i+1, j+1)
+			color := "#ff0000"
+			if peak {
+				color = "#0000ff"
+			}
+			s = fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g' style='fill:%s'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy, color)
+			buffer.WriteString(s)
+		}
+	}
+	s = fmt.Sprintln("</svg>")
+	buffer.WriteString(s)
+	out.Write(buffer.Bytes())
+}
+
+func corner(i, j int) (float64, float64, bool) {
+	// Find point (x,y) at corner of cell (i,j).
+	x := xyrange * (float64(i)/cells - 0.5)
+	y := xyrange * (float64(j)/cells - 0.5)
+
+	// Compute surface height z.
+	z := f(x, y)
+	peak := true
+	if z < 0 {
+		peak = false
+	}
+
+	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
+	sx := width/2 + (x-y)*cos30*xyscale
+	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	return sx, sy, peak
+}
+
+func f(x, y float64) float64 {
+	r := math.Hypot(x, y) // distance from (0,0)
+	if r != 0 {
+		return math.Sin(r) / r
+	}
+	return 0
+}
+
+//!-handler

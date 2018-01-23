3e42a79f82bda2d23ec7eeb89830f470353abfd4
jiujieti <melody2014ymq@gmail.com>
2018-01-23 22:57:57 +0100

    3.7 added

---
+++ ch3/3.7/main.go
@@ -0,0 +1,47 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 61.
+//!+
+
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, newton(z))
+		}
+	}
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+func newton(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	for n := uint8(0); n < iterations; n++ {
+		if z == 1 || z == -1 || z == 1i || z == -1i {
+			return color.Gray{255 - contrast*n}
+		}
+		z -= (z*z*z*z - 1) / (4 * z * z * z)
+	}
+	return color.Black
+}

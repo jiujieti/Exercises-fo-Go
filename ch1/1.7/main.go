251ff4f31a26ff8bc75f3eb8b86b14184381abb4
jiujieti <melody2014ymq@gmail.com>
2020-07-25 21:38:55 +0100

    fix 1.7

--- ch1/1.7/main.go
+++ ch1/1.7/main.go
@@ -21,13 +21,16 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := io.Copy(os.Stdout, resp.Body)
+		n, err := io.Copy(os.Stdout, resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "io: copy %v\n", err)
+		}
+		fmt.Printf("copied %d bytes\n", n)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", string(b))
 	}
 }
 

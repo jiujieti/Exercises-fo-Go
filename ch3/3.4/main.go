dd2d853f6fd6595298ae744a2fb094ff702c294b
jiujieti <melody2014ymq@gmail.com>
2020-08-07 00:12:44 +0100

    Add 3.4

--- ch3/3.4/main.go
+++ ch3/3.4/main.go
@@ -1,93 +1,110 @@
 // Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
-// See page 21.
+// See page 58.
+//!+
 
-// Server3 is an "echo" server that displays request parameters.
+// Surface computes an SVG rendering of a 3-D surface function.
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"log"
 	"math"
 	"net/http"
+	"strconv"
 )
 
 const (
-	width, height = 600, 320            // canvas size in pixels
-	cells         = 100                 // number of grid cells
-	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
-	xyscale       = width / 2 / xyrange // pixels per x or y unit
-	zscale        = height * 0.4        // pixels per z unit
-	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+	cells   = 100         // number of grid cells
+	xyrange = 30.0        // axis ranges (-xyrange..+xyrange)
+	angle   = math.Pi / 6 // angle of x, y axes (=30°)
 )
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+
+var width float64 = 600
+var height float64 = 320
+var color = "#454545"
 
 func main() {
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-//!+handler
-// handler echoes the HTTP request.
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/svg+xml")
+	if err := r.ParseForm(); err != nil {
+		log.Println(err)
+	}
+
+	for k, v := range r.Form {
+		if k == "width" {
+			w, e := parse(v)
+			if e == nil {
+				width = w
+			}
+		}
+		if k == "height" {
+			h, e := parse(v)
+			if e == nil {
+				height = h
+			}
+		}
+		if k == "color" {
+			color = v[0]
+		}
+	}
 	surface(w)
 }
 
-func surface(out io.Writer) {
-	buffer := new(bytes.Buffer)
-	s := fmt.Sprintf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
-		"width='%d' height='%d'>", width, height)
-	buffer.WriteString(s)
-	for i := 0; i < cells; i++ {
-		for j := 0; j < cells; j++ {
-			ax, ay, _ := corner(i+1, j)
-			bx, by, peak := corner(i, j)
-			cx, cy, _ := corner(i, j+1)
-			dx, dy, _ := corner(i+1, j+1)
-			color := "#ff0000"
-			if peak {
-				color = "#0000ff"
-			}
-			s = fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g' style='fill:%s'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy, color)
-			buffer.WriteString(s)
-		}
+func parse(v []string) (float64, error) {
+	value, e := strconv.ParseFloat(v[0], 64)
+	if e != nil {
+		log.Println(e)
 	}
-	s = fmt.Sprintln("</svg>")
-	buffer.WriteString(s)
-	out.Write(buffer.Bytes())
+	return value, e
 }
 
-func corner(i, j int) (float64, float64, bool) {
+func surface(out io.Writer) {
+	fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", int(width), int(height))
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			ax, ay := corner(i+1, j)
+			bx, by := corner(i, j)
+			cx, cy := corner(i, j+1)
+			dx, dy := corner(i+1, j+1)
+			fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g' style='fill:%s'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy, color)
+		}
+	}
+	fmt.Fprintf(out, "</svg>")
+}
+
+func corner(i, j int) (float64, float64) {
+
+	var xyscale = float64(width) / 2 / xyrange
+	var zscale = float64(height) * 0.4
+
 	// Find point (x,y) at corner of cell (i,j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
 	// Compute surface height z.
 	z := f(x, y)
-	peak := true
-	if z < 0 {
-		peak = false
-	}
 
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-	return sx, sy, peak
+	return sx, sy
 }
 
 func f(x, y float64) float64 {
 	r := math.Hypot(x, y) // distance from (0,0)
-	if r != 0 {
-		return math.Sin(r) / r
-	}
-	return 0
+	return math.Sin(r) / r
 }
 
-//!-handler
+//!-

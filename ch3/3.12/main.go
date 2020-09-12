2d08aef33563890015a8ecc9f153a136c77b340d
jiujieti <melody2014ymq@gmail.com>
2020-09-12 15:01:05 +0100

    Add 3.12

--- ch3/3.12/main.go
+++ ch3/3.12/main.go
@@ -7,20 +7,23 @@ import (
 
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Println("Please give two strings.")
+		fmt.Println("Please enter two strings!")
 		return
 	}
-	isAna := isAnagram(os.Args[1], os.Args[2])
-	fmt.Printf("%t\n", isAna)
+	var isAna = ""
+	if !isAnagram(os.Args[1], os.Args[2]) {
+		isAna = " not"
+	}
+	fmt.Printf("%s and %s are%s anagrams of each other.\n", os.Args[1], os.Args[2], isAna)
 }
 
 func isAnagram(s1, s2 string) bool {
 	m := make(map[rune]int)
-	for _, s := range s1 {
-		m[s]++
+	for _, r := range s1 {
+		m[r]++
 	}
-	for _, s := range s2 {
-		m[s]--
+	for _, r := range s2 {
+		m[r]--
 	}
 	for _, v := range m {
 		if v != 0 {
